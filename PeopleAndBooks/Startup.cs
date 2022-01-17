ada9660cd9f86db83a8bd2356dc421b3e54eab0a
Helbert Moreira <helbertmspro@gmail.com>
2022-01-17 14:33:17 -0300

    Content Negociation - Para a aplicação aceitar requisições também no formato de XML

--- PeopleAndBooks/Startup.cs
+++ PeopleAndBooks/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Net.Http.Headers;
 using PeopleAndBooks.Business;
 using PeopleAndBooks.Business.Implementations;
 using PeopleAndBooks.Data;
@@ -28,7 +29,16 @@ namespace PeopleAndBooks
 
             services.AddDbContext<SqlServerContext>(options =>
                                 options.UseSqlServer(Configuration.GetConnectionString("SqlServerContext")));
-            
+
+            //Content Negociation
+            //Service adicionado para que a aplicação aceite xml
+            services.AddMvc(options =>
+            {
+                options.RespectBrowserAcceptHeader = true;
+                options.FormatterMappings.SetMediaTypeMappingForFormat("xml", MediaTypeHeaderValue.Parse("application/xml"));
+                options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
+            })
+            .AddXmlSerializerFormatters();
 
             // Versionamento da API - Precisa do nuget Microsoft.AspnetCore.Mvc.Versioning
             services.AddApiVersioning();
